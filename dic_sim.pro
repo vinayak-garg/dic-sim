8cc8e913624854ff1312735f264e53a6130fe14c
Vinayak Garg <vinayakgarg2.0@gmail.com>
2012-10-25 11:59:33 +0530

    Testing Backgound for QMainWindow

--- dic_sim.pro
+++ dic_sim.pro
@@ -14,3 +14,6 @@ SOURCES += main.cpp\
         mainwindow.cpp
 
 HEADERS  += mainwindow.h
+
+RESOURCES += \
+    resource.qrc
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 
+#include <QtGui>
 #include <QApplication>
 #include <QAction>
 #include <QMenuBar>
@@ -8,7 +9,9 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    /****************** Menu Bar ******************/
     QAction *newAction = new QAction("&New File", this);
+    newAction->setShortcut(tr("Ctrl+N"));
     QAction *quitAction = new QAction("&Quit", this);
     quitAction->setShortcut(tr("Ctrl+Q"));
 
@@ -18,6 +21,21 @@ MainWindow::MainWindow(QWidget *parent)
     fileMenu->addAction(quitAction);
 
     connect(quitAction, SIGNAL(triggered()), qApp, SLOT(quit()));
+
+    QAction *aboutAction = new QAction("&About", this);
+
+    QMenu *helpMenu;
+    helpMenu = menuBar()->addMenu("&Help");
+    helpMenu->addAction(aboutAction);
+
+    /*************** Central Contents ***************/
+    /*
+    QPalette* palette = new QPalette();
+    palette->setBrush(QPalette::Background, *(new QBrush( *(new QPixmap("images/textures/icobbg1.png")))));
+    setPalette(*palette);
+    */
+
+    setStyleSheet("background-image: url(images/textures/icobbg1.png);");
 }
 
 MainWindow::~MainWindow()
